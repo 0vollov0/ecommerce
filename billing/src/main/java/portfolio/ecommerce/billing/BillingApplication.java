58931f03ea27b80cb5b9b5d8d4fd8d2d9cd4791a
SinGyu Park <psk876@naver.com>
2025-01-07 15:26:07 +0900

    Initial commit

---
+++ billing/src/main/java/portfolio/ecommerce/billing/BillingApplication.java
@@ -0,0 +1,16 @@
+package portfolio.ecommerce.billing;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingApplication {
+	private static final Logger log = LogManager.getLogger();
+	public static void main(String[] args) {
+		log.info("billing application is running....");
+		SpringApplication.run(BillingApplication.class, args);
+	}
+
+}
